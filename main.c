ceb6f88c6304a6167c6fed8c68edca79c7b92a24
franjunoy <franjunoy@hotmail.com>
2023-09-24 17:34:38 -0300

    Funcion Main

--- main.c
+++ main.c
@@ -64,3 +64,59 @@ int ntamao(cola *inicio){
 int frente(cola *inicio){
     return inicio->dato;
 }
+int main()
+{
+    cola *inicio;
+    cola *fin;
+    inicio=NULL;
+    fin=NULL;
+    do{
+        printf("Menu\n");
+        printf("1) Agregar un elemento\n");
+        printf("2) Eliminar y retornar el primer elemento\n");
+        printf("3) Retornar el primer elemento sin eliminarlo\n");
+        printf("4) Vaciar la cola\n");
+        printf("5) Cantidad de elementos\n");
+        printf("6) Saber si hay o no elementos\n");
+        scanf("%d", &opcion);
+
+        switch(opcion){
+            case 1:
+                printf("Ingrese un elemento\n");
+                scanf("%d", &a);
+                encola(&inicio, &fin, a);
+                break;
+            case 2:
+                b=desencola(&inicio, &fin);
+                if (b==0){
+                    printf("La cola no tiene elementos\n");
+                }else{
+                    printf("Se elimino el elemento: %d\n", b);
+                }
+            break;
+            case 3:
+                printf("El primer valor es: %d\n", frente(inicio));
+                break;
+            case 4:
+                vaciaCola(&inicio);
+                fin=NULL;
+                break;
+            case 5:
+                printf("La cantidad de elementos que posee la pila es: %d\n", ntamao(inicio));
+                break;
+            case 6:
+                c=estaVacia(&fin);
+                if(c==0){
+                    printf("La cola esta vacia\n");
+                }else{
+                    printf("La cola posee elementos\n");
+                }
+                break;
+            default:
+                printf("Opcion incorrecta\n");
+        }
+
+    }
+    while(opcion!=0);
+    return 0;
+}
